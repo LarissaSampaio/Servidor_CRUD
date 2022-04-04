9ce9fae9d195c32a95db7f61205b375166b350b0
LarissaSampaio <larisampaio@hotmail.com>
2022-04-04 18:13:05 -0300

    Prévia - ainda com erros

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const clientes = [];
+const clientes = require('./banco-dados');
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/clientes', (req,res) => {
-    return res.json(clientes);
+   res.json(clientes);
 });
 
-app.get('/clientes/:index', (req, res) => {
-    return res.json(req.user);
-})
+app.get('/clientes/:id', (req, res) => {
+    res.json(req.user)
+});
 
 app.post('/clientes', (req, res) => {
-    const { name } = req.body;
-    clientes.push(name);
-    return res.json(clientes);
+    const {newClient} = req.body;
+    clientes.push(newClient);
+    res.json(clientes);
 })
 
 app.patch('/clientes/:index', (req, res) => {
     const{ index } = req.params;
     const { name } = req.body;
     clientes[index] = name;
-    return res.json(clientes);
+    res.json(clientes);
 })
 
 app.delete('/clientes/:index', (req, res) => {
     const{ index } = req.params;
     clientes.splice(index, 1);
-    return res.send();
+    res.send();
 })
 
-app.listen(3000);
+app.listen(port);
